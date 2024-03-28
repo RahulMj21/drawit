d545ffb34ebe2e3867933dd9ee93443a821f5c55
Rahul Mondal <rahulmondar@gmail.com>
2024-03-28 09:10:07 +0530

    feat: updated header in team/create page

--- src/app/(routes)/team/create/page.tsx
+++ src/app/(routes)/team/create/page.tsx
@@ -1,25 +1,29 @@
 import CreateTeamForm from "@/components/forms/createTeamForm";
+import GradientBg from "@/components/home/gradientBg";
 import Logo from "@/components/navigation/logo";
-import LogoutButton from "@/components/navigation/logoutButton";
+import { ModeToggle } from "@/components/ui/modeToggle";
 
 const Create = () => {
   return (
-    <div className="container">
-      <header className="flex items-center justify-between h-20">
-        <Logo />
-        <LogoutButton />
-      </header>
-      <main className="flex flex-col gap-6 py-16 text-center">
-        <h1 className="text-3xl text-gray-700 font-extrabold sm:text-5xl">
-          What should we call
-          <br /> your team?
-        </h1>
-        <h2 className="text-gray-600/80">
-          You can change it anytime from the settings.
-        </h2>
-        <CreateTeamForm />
-      </main>
-    </div>
+    <>
+      <GradientBg />
+      <div className="container">
+        <header className="flex items-center justify-between h-20">
+          <Logo />
+          <ModeToggle />
+        </header>
+        <main className="flex flex-col gap-6 py-16 text-center">
+          <h1 className="text-3xl text-secondary-foreground/80 font-extrabold sm:text-5xl">
+            What should we call
+            <br /> your team?
+          </h1>
+          <h2 className="text-muted-foreground">
+            You can change it anytime from the settings.
+          </h2>
+          <CreateTeamForm />
+        </main>
+      </div>
+    </>
   );
 };
 
